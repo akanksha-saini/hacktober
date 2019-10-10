47b008dd838a63486b866e58f6e0506368906c6b
Akanksha Saini <37983977+akanksha-saini@users.noreply.github.com>
2019-10-11 01:13:19 +0530

    Update circle.cpp
    added comment to improve understandibility

--- circle.cpp
+++ circle.cpp
@@ -10,9 +10,9 @@ int main()
 	scanf("%d",&x);
 	printf("\ny=");
 	scanf("%d",&y);
-	if(((x*x)+(y*y))>(r*r))
+	if(((x*x)+(y*y))>(r*r)) //to check whether square of radius is less than the sum of squares of x and y then point lies outside the circle
 		printf("(%d,%d) lies outside the circle",x,y);
-	else if(((x*x)+(y*y))<(r*r))
+	else if(((x*x)+(y*y))<(r*r))//to check whether square of radius is greater than the sum of squares of x and y then point lies inside the circle
 			printf("(%d,%d) lies inside the circle",x,y);
 		else
 			printf("(%d,%d) lies on the circle",x,y);
